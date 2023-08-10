6a79b7ef095daa4387ca35781852941cea5ec723
mkaran12 <mosesjnr46466@gmail.com>
2023-08-10 15:54:51 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdion.h>
+ /**The preprocessor directive #include <stdio/h>
+  * the entry point -main
+  **/
+int main(){
+	/**Declaration of string x **/
+	char X [] = "Programming is like building a multilingual puzzle,";
+	/**outputing the output of x **/
+	put ("%s", x)	
+	/**Returning the value of 0 **/
+	return 0;
+}
