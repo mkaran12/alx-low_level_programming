f172e38b17bc470d36ab69854c55fc96e583d32c
mkaran12 <mosesjnr46466@gmail.com>
2023-08-16 18:27:24 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets in lowercase.
+ * Return: On success 1.
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++);
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
