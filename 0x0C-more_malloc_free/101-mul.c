287db683d0f994377a329634f2a51c9b77ee186c
Replit user <>
2023-09-08 00:18:16 +0000

    changes to spaciong to obey betty

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,7 +30,6 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
 	int i = 0;
@@ -45,7 +44,6 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
-
 void errors(void)
 {
 	printf("Error\n");
@@ -59,7 +57,6 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
