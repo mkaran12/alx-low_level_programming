afd1c8a6ca2de2f643f78befc66300608c9b3d56
Replit user <>
2023-09-08 00:28:12 +0000

    changes in spacing of comment message to obey betty

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,6 +30,7 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
@@ -44,6 +45,7 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
+
 void errors(void)
 {
 	printf("Error\n");
@@ -57,6 +59,7 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
